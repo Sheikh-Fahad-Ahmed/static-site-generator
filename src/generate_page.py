96d8b6786dd584f4600906fb898165d1c7708cee
Fahad Ahmed <fahad838@gmail.com>
2025-03-09 11:17:01 +0530

    Change dest_dir from public to docs for github pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ import shutil
 from block_markdown import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -20,23 +20,26 @@ def generate_page(from_path, template_path, dest_path):
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
 
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
+
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(template)
 
-def generate_page_recursively(dir_path_content, template_content, dest_dir_path):
+def generate_page_recursively(dir_path_content, template_content, dest_dir_path, basepath):
     dir_list = os.listdir(dir_path_content)
     for filename in dir_list:
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         print(f" * {from_path} -> {dest_path}")
         if os.path.isdir(from_path):
-            generate_page_recursively(from_path, template_content, dest_path)
+            generate_page_recursively(from_path, template_content, dest_path, basepath)
         elif filename.endswith(".md"):
             dest_path = dest_path.replace(".md", ".html")
-            generate_page(from_path, template_content, dest_path)
+            generate_page(from_path, template_content, dest_path, basepath)
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,28 +1,36 @@
 import os
 import shutil
+import sys
 
 from copy_static_to_public import copy_files_recursive
 from generate_page import generate_page, generate_page_recursively
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
-    print("Deleting public directory...")
+    if len(sys.argv) < 2:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
+    
+
+    print("Deleting docs directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static files to public directory...")
+    print("Copying static files to docs directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating page...")
     generate_page_recursively(
         dir_path_content,
         template_path,
-        dir_path_public
+        dir_path_public,
+        basepath
     )
 
 
